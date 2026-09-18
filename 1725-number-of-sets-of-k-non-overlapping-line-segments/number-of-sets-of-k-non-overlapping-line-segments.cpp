f5a5bd57151c27926135ecbc4ad974b2f89d7282
AdityaKumar10805 <adityak.ug24.cs@nitp.ac.in>
2026-09-19 02:00:31 +0530

    Time: 59 ms (26.73%) | Memory: 53.3 MB (18.41%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-
+    const int MOD=1e9+7;
+    int solve(int i,int curr,int &n,int &k,vector<vector<int>>&dp){
+        if(curr==k){
+            return 1;
+        }
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i][curr]!=-1){
+            return dp[i][curr];
+        }
+        int  notTake=solve(i+1,curr,n,k,dp)%MOD;
+        int take=0;
+        for(int j=i+1;j<n;j++){
+           take+= (solve(j,curr+1,n,k,dp)%MOD);
+           take=take%MOD;
+        }
+        int ans=(1LL*take+notTake)%MOD;
+        return dp[i][curr]= ans;
+    }
     int numberOfSets(int n, int k) {
-        vector<vector<long long>> dp(n, vector<long long>(k + 1));
-        // 0 segments → exactly 1 way
-        for(int i = 0; i < n; i++) dp[i][0] = 1;
+        vector<vector<int>>dp(n+1,vector<int>(k+1,0));
+        //bottom up approach
+        for(int i=0;i<n;i++){
+            dp[i][k]=1;
+        }
+        for(int i=0;i<=k;i++){
+            dp[n][i]=0;
+        }
+        //
+        for(int j=k-1;j>=0;j--){
+            vector<int>suffix(n+1,0);
+            suffix[n-1]=dp[n-1][j+1];
+            for(int l=n-2;l>=0;l--){
+                suffix[l]=(suffix[l+1]+dp[l][j+1])%MOD;
+            }
+            for(int i=n-1;i>=0;i--){
+                int notTake=dp[i+1][j];
+                int take=0;
+                // for(int k=i+1;k<n;k++){
+                //     take+=dp[k][j+1];
+                //     take=take%MOD;
+                // }
+                take=suffix[i+1];
 
-        for(int j = 1; j <= k; j++){
-            long long sum = 0;
-            for(int i = 1; i < n; i++){
-                // Add ways for j-1 segments
-                sum = (sum + dp[i - 1][j - 1]) % MOD;
-                // Don't use i OR end a segment at i
-                dp[i][j] = (dp[i - 1][j] + sum) % MOD;
+                int ans=(1LL*take+notTake)%MOD;
+                dp[i][j]=ans;
             }
         }
-
-        return dp[n - 1][k];
+        return dp[0][0];
+        
     }
 };
